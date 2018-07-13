e572e1fee44ace7072f9da04be7a8b5da988bd2a
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-07-13 18:48:10 +0900

    wip: experimented with updateElement and helper functions
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Tsubasa Kondo <tsubasa.k111@gmail.com>

--- app.js
+++ app.js
@@ -42,10 +42,79 @@ function changed(node1, node2) {
   );
 }
 
+function getDiff(newNode, oldNode) {
+  let diff = {};
+
+  // console.log(changed(newNode, oldNode));
+  // console.log(newNode.props);
+  // console.log(oldNode.props);
+  // for(key in newNode.props){
+  if (!newNode.props || Object.entries(newNode.props) > 0) {
+    diff.props = {};
+    let newPropEntries = Object.entries(newNode.props);
+
+    Object.keys(newNode.props).forEach((key) => {
+      if (changed(newNode.props[key], oldNode.props[key])) {
+        diff.props[key] = newNode.props[key];
+      }
+      console.log(key);
+      // console.log(newNode.props[key], oldNode.props[key]);
+      // console.log(changed(newNode.props[key], oldNode.props[key]));
+    });
+  }
+
+  // }
+  // console.log(changed(newNode.props, oldNode.props));
+
+  if (newNode.children && oldNode.children) {
+    let iIterator = Math.min(newNode.children.length, oldNode.children.length);
+    for (let i = 0; i < iIterator; i++) {
+      getDiff(newNode.children[i], oldNode.children[i]);
+    }
+  }
+
+  // if(newNode.children && oldNode.children){
+  //   for(let nIterator = 0; nIterator < newNode.children.length; nIterator++){
+  //     for(let oIterator = 0; oIterator < oldNode.children.length; oIterator++){
+  //       if(nIterator === oIterator){
+  //         diff(newNode.children[nIterator], oldNode.chidlren[oIterator]);
+  //       }
+  //     }
+  //   }
+  // }
+
+  return diff;
+}
+
+function applyUpdate(target, diff) {
+  if (diff.props) {
+    for (let key of diff.props) {
+      target.setAttribute(key, diff.props[key]);
+    }
+  }
+  if (diff.children) {
+    diff.children.forEach((child) => {
+      let newChild;
+      if (typeof child === "string") {
+        newChild = document.createTextNode(child);
+      } else {
+        newChild = recurse(child);
+      }
+      target.appendChild(newChild);
+    });
+  }
+  return target;
+}
+
 function updateElement(target, newNode, oldNode) {
   // compare newNode to oldNode
+  let diff = getDiff(newNode, oldNode);
+  // console.log(diff);
+
   // write differences to newNode
-  // copy newNode to target
+  // applyUpdate(target, diff);
+
+  // append differences to target
   let newElement = createElement("span");
   target.appendChild(newElement);
 }
